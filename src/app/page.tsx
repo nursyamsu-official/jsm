3554002d1388259639a435a1cc5f4ff153c0ced2
Nur Syamsu <nursyamsu.official@gmail.com>
2024-10-30 09:35:53 +0700

    ESLint & Prettier Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-  <div>Learning Next JS</div>
-  );
+  return <div className="text-3xl font-black text-white">Learning Next JS</div>;
 }
